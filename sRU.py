8786e0abbc727130a17e1be0f9fe863ccb201b07
TidB <tidb@gmx.net>
2014-08-13 13:52:23 +0200

    Russian support

---
+++ sRU.py
@@ -0,0 +1,199 @@
+#NAME
+LANGUAGE_NAME = "Russian"
+
+#STANDARD
+ADDEDTOGAME = "{{{{item name|{0}}}}} был добавлен в игру."
+ALLCLASSESBOX = "[[Classes/ru|Всеми классами]]"
+DISPLAYTITLE = "{{DISPLAYTITLE: {{{{item name|{0}}}}}"
+ITEMLOOK = " Представляет собой"
+LEVEL = "{{1}} {{0}}-го уровня"
+NOUNMARKER_INDEFINITE_COSMETIC = " "
+NOUNMARKER_INDEFINITE_SET = ""
+NOUNMARKER_INDEFINITE_WEAPON = ""
+
+SENTENCE_1_ALL = "'''{{{{item name|{0}}}}}''' ­- это {1}{2}{3} {4} для {5}."
+SENTENCE_1_COMMUNITY_COSMETIC = "[[Steam Workshop/ru|созданный сообществом]]"
+SENTENCE_1_COMMUNITY_WEAPON = "[[Steam Workshop/ru|созданное сообществом]]"
+SENTENCE_1_PROMO_COSMETIC = "[[Promotional items/ru|поощрительный]]"
+SENTENCE_1_PROMO_WEAPON = "[[Promotional items/ru|поощрительное]]"
+SENTENCE_1_COSMETIC = "[[Cosmetic item/ru|аксессуар]]"
+SENTENCE_1_SET = "[[Item set/ru|набор предметов]]"
+SENTENCE_1_CLASSES_ALL = "всех [[Classes/ru|классов]]"
+SENTENCE_1_CLASSES_ONE = "[[{0}]]"
+SENTENCE_1_CLASSES_MORE = "и [[{0}]]"
+SENTENCE_1_CLASSES_AND = ", "
+SENTENCE_1_CLASSES_COMMA = " и "
+
+SENTENCE_COMMUNITY = "Этот предмет был {1} в [[Steam Workshop/ru|Мастерской Steam]]{2}."
+SENTENCE_COMMUNITY_LINK = "[{0} представлен]"
+SENTENCE_COMMUNITY_NAME = "под названием «{0}»"
+
+SENTENCE_PROMOTIONAL = "{{{{item name|{0}}}}} [[Genuine/ru|высшей пробы]] были награждены игроки, купившие {1}{2}{3}."
+SENTENCE_PROMOTIONAL_STEAM = "в [[Steam/ru|Steam]]"
+SENTENCE_PROMOTIONAL_DATE = "до {0}"
+
+SENTENCE_SET = "Предмет был добавлен в игру в {{{{Update link|{0}}}}}."
+SENTENCE_SET_INCLUDES = "Этот набор включает в себя следующие предметы:"
+
+SENTENCE_THUMBNAIL = "Миниатюрное изображение {{{{item name|{0}}}}} в Мастерской Steam"
+
+SENTENCE_1_SUB_PRIMARY = "[[Weapon/ru#{0}primary|основное оружие]]"
+SENTENCE_1_SUB_SECONDARY = "[[Weapon/ru#{0}secondary|дополнительное оружие]]"
+SENTENCE_1_SUB_MELEE = "оружие [[Weapon/ru#{0}melee|ближнего боя]]"
+
+#FLAGS
+DICTIONARY_FLAGS = {
+    "not usable in crafting" : "Нельзя перековывать",
+    "not tradable" : "Нельзя передавать",
+    "not tradable or usable in crafting" : "Нельзя передавать или перековывать",
+}
+
+#ATTRIBUTES
+DICTIONARY_ATTS = {
+    "achievement item: not tradable" : "За выполненное достижение: нельзя передавать",
+    "holiday restriction: tf birthday" : "Можно использовать только на День рождения TF",
+    "holiday restriction: winter" : "Можно использовать только зимой",
+    "holiday restriction: halloween" : "Можно использовать только на Хеллоуин",
+    "holiday restriction: halloween / full moon" : "Можно использовать только на Хеллоуин и в полнолуние",
+    "holiday restriction: halloween / full moon / valentine's day" : "",
+}
+
+#CLASSES
+DICTIONARY_CLASSES = {
+    "Scout" : "разведчика",
+    "Soldier" : "солдата",
+    "Pyro" : "поджигателя",
+    "Demoman" : "подрывника",
+    "Heavy" : "пулеметчика",
+    "Engineer" : "инженера",
+    "Medic" : "медика",
+    "Sniper" : "снайпера",
+    "Spy" : "шпиона",
+}
+
+#HEADLINES
+DICTIONARY_HEADLINES = {
+    'as a crafting ingredient' : "Как компонент при ковке",
+    'blueprint' : "Чертеж",
+    'bugs' : "Ошибки",
+    'crafting' : "Ковка",
+    'damage and function times' : "Урон и время",
+    'external links' : "Ссылки",
+    'gallery' : "Галерея",
+    'item set' : "Набор предметов",
+    'notes' : "Заметки",
+    'painted variants' : "Варианты покраски",
+    'references' : "Примечания",
+    'related achievements' : "Связанные достижения",
+    'see also' : "См. также",
+    'strange variant' : "Вариант странного типа",
+    'styles' : "Стили",
+    'trivia' : "Факты",
+    'unused content' : "Неиспользуемый контент",
+    'update history' : "Предыдущие изменения",
+}
+
+#ITEM LEVELS
+DICTIONARY_LEVEL_C = {
+    'Apparel' : "Одежда",
+    'Armband' : "Повязка на руку",
+    'Aura of Incorruptibility' : "Аура неподкупности",
+    'Backpack' : "Рюкзак",
+    'Badge' : "Значок",
+    'Balloon' : "Воздушный шарик",
+    'Bandages' : "Бинты",
+    'Bandana' : "Бандана",
+    'Bandolier' : "Патронташ",
+    'Barbeque' : "Барбекю",
+    'Beach Towel' : "Пляжное полотенце",
+    'Bells' : "Бубенцы",
+    'Bird Head' : "Голова птицы",
+    'Blueprints' : "Чертежи",
+    'Bones' : "Кости",
+    'Bongos' : "Бонго",
+    'Boots' : "Ботинки",
+    'Botkiller' : "ботоубийцы ",
+    'Cape' : "Мантия",
+    'Championship Belt' : "Чемпионский пояс",
+    'Cigar' : "Сигара",
+    'Coat' : "Пальто",
+    'Coffin' : "",
+    'Community Medal' : "Медаль сообщества",
+    'Conscience' : "Совесть",
+    'Cooler' : "Холодильник",
+    'Cosmetic Armor' : "Бутафорская броня",
+    'Cosmetic Augmentation' : "Фальшивое улучшение",
+    'Cosmetic Axe' : "Бутафорский топор",
+    'Cosmetic Knife' : "Бутафорский нож",
+    'Costume Piece' : "Часть костюма",
+    'Decorative Bombs' : "Декоративные бомбы",
+    'Duck' : "Утка",
+    'Electronic Device' : "Электронное устройство",
+    'Eye Stalks' : "Глаза на стебельках",
+    'Facial Hair' : "Растительность на лице",
+    'Flair!' : "Значки",
+    'Flip-Flops' : "Шлепанцы",
+    'Fuel Tank' : "Бак с топливом",
+    'Func_Medal' : "Func_Medal",
+    'Futuristic Sound Device' : "Футуристическое устройство для воспроизведения звука",
+    'Ghost' : "Призрак",
+    'Glasses' : "Очки",
+    'Glove' : "Перчатка",
+    'Gloves' : "Боксерские перчатки",
+    'Golf Clubs' : "Клюшки для гольфа",
+    'Hair' : "Прическа",
+    'Hat' : "Головной убор",
+    'Headgear' : "Головной убор",
+    'Headset' : "Гарнитура",
+    'Helmet' : "Шлем",
+    'Holiday Hat' : "Праздничная шляпа",
+    'Hooves' : "Копыта",
+    'Kilt' : "Килт",
+    'Lantern' : "Фонарь",
+    'Lunchbox' : "Коробка для завтраков",
+    'Mascot' : "Талисман",
+    'Mask' : "Маска",
+    'Medal' : "Медаль",
+    'Medallion' : "Медальон",
+    'Mystical Lamp' : "Таинственная лампа",
+    'Necklace' : "Шея",
+    'Photograph' : "Фотография",
+    'Pin' : "Значок",
+    'Pipe' : "Трубка",
+    'Pocket Buddy' : "Карманный дружок",
+    'Pocket Square' : "Карманный платок",
+    'Poncho' : "Пончо",
+    'Puffy Shirt' : "Пышная рубашка",
+    'Pyrovision Goggles' : "Очки Пирозрения",
+    'Refreshment' : "Освежающий напиток ",
+    'Ring' : "Кольцо",
+    'Robot' : "Робот",
+    'Safety Apparatus' : "Защитный прибор",
+    'Satchel' : "Сумка",
+    'Scarf' : "Шарф",
+    'Science Project' : "",
+    'Shield' : "Щит",
+    'Shirt' : "Рубашка",
+    'Shoes' : "Обувь",
+    'Skateboard' : "Скейтборд",
+    'Sled' : "Сани",
+    'Snow Globe' : "Снежный шар",
+    'Spikes' : "Шипы",
+    'Spirit Animal' : "Призрачный зверь",
+    'Spooky Companion' : "",
+    'Spurs' : "Шпоры",
+    'Squirrel' : "",
+    'Stethoscope' : "Стетоскоп",
+    'Stocking' : "Носок",
+    'Supplies' : "Запасы",
+    'Tattoos' : "Татуировки",
+    'Tentacles' : "Щупальца",
+    'Tournament Medal' : "Медаль турнира",
+    'Towel' : "Полотенце",
+    'Treasure' : "Сокровищница",
+    'Tuxedo' : "Смокинг",
+    'Undead Pet' : "Питомец-нежить",
+    'Uniform' : "Униформа",
+    "Veteran's Beret" : "Берет ветерана",
+    'Wings' : "Крылья",
+    }

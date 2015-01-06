b5e6d0ac5a7fd557056b31145ea1610888be6892
TidB <tidb@gmx.net>
2015-01-06 21:11:47 +0100

    +dutch

---
+++ sNL.py
@@ -0,0 +1,191 @@
+#NAME
+LANGUAGE_NAME = "Nederlands"
+
+#STANDARD
+ADDEDTOGAME = "Dit voorwerp is toegevoegd aan het spel."
+ALLCLASSESBOX = "[[Classes/nl|Alle klassen]]"
+DISPLAYTITLE = "{{{{DISPLAYTITLE: {{{{item name|{0}}}}}"
+ITEMLOOK = " Het ziet er uit als"
+LEVEL = "Level {0} {1}"
+NOUNMARKER_INDEFINITE_COSMETIC = "een"
+NOUNMARKER_INDEFINITE_SET = "een"
+NOUNMARKER_INDEFINITE_WEAPON = "een"
+
+SENTENCE_1_ALL = "(NOUN MARKER) '''{{{{item name|{0}}}}}''' is {1}{2}{3} {4} voor {5}."
+SENTENCE_1_COMMUNITY_COSMETIC = " [[Steam Workshop/nl|community-gemaakt]]"
+SENTENCE_1_COMMUNITY_WEAPON = " [[Steam Workshop/nl|community-gemaakt]]"
+SENTENCE_1_PROMO_COSMETIC = " [[Promotional items/nl|promotioneel]]"
+SENTENCE_1_PROMO_WEAPON = " [[Promotional items/nl|promotioneel]]"
+SENTENCE_1_COSMETIC = "[[Cosmetic item/nl|Cosmetisch voorwerp]]"
+SENTENCE_1_SET = " [[Item set/nl|Voorwerp set]]"
+SENTENCE_1_CLASSES_ALL = "alle [[Classes/nl|Klassen]]"
+SENTENCE_1_CLASSES_ONE = "(Article) [[{0}/nl|{0}]]"
+SENTENCE_1_CLASSES_MORE = "en (Article) [[{0}/nl|{0}]]."
+SENTENCE_1_CLASSES_AND = ","
+SENTENCE_1_CLASSES_COMMA = " en "
+
+SENTENCE_COMMUNITY = "Dit voorwerp is {0} aan de [[Steam Workshop/nl|Steam Workshop]]{1}."
+SENTENCE_COMMUNITY_LINK = " [{0} toegevoegd]"
+SENTENCE_COMMUNITY_NAME = 'onder de naam "{0}"'
+
+SENTENCE_PROMOTIONAL = "Dit voorwerp is gegeven in [[Genuine/nl|Authentieke]] kwaliteit aan de spelers die (article) ''[[{0}/nl|{0}]]'' kochten {1}{2}."
+SENTENCE_PROMOTIONAL_STEAM = "op [[Steam/nl|Steam]]"
+SENTENCE_PROMOTIONAL_DATE = " voor {0}"
+
+SENTENCE_SET = "Het was toegevoegd aan het spel in (article) {{{{Update link|{0}}}}}."
+SENTENCE_SET_INCLUDES = "De set bevat volgende voorwerpen:"
+
+SENTENCE_THUMBNAIL = "|Steam workshop afbeelding voor dit voorwerp"
+
+SENTENCE_1_SUB_PRIMARY = "[[Weapon/nl#{0}primary|primair wapen]]"
+SENTENCE_1_SUB_SECONDARY = "[[Weapon/nl#{0}secondary|secondair wapen]]"
+SENTENCE_1_SUB_MELEE = "[[Weapon/nl#{0}melee|melee wapen]]"
+
+#FLAGS
+DICTIONARY_FLAGS = {
+    "not usable in crafting" : "Niet te gebruiken bij ontwerpen",
+    "not tradable" : "Niet ruilbaar",
+    "not tradable or usable in crafting" : "Niet ruilbaar of te gebruiken bij ontwerpen",
+}
+
+#ATTRIBUTES
+DICTIONARY_ATTS = {
+    "achievement item: not tradable" : "Prestatievoorwerp: niet ruilbaar",
+    "holiday restriction: tf birthday" : "Feestdagbeperking: verjaardag van TF",
+    "holiday restriction: winter" : "Feestdagbeperking: Winter",
+    "holiday restriction: halloween" : "Feestdagbeperking: Halloween",
+    "holiday restriction: halloween / full moon" : "Feestdagbeperking: Halloween / Volle maan",
+    "holiday restriction: halloween / full moon / valentine's day" : "Feestdagbeperking: Halloween / Volle maan / Valentijnsdag",
+}
+
+#CLASSES
+DICTIONARY_CLASSES = {
+
+}
+
+#HEADLINES
+DICTIONARY_HEADLINES = {
+    'as a crafting ingredient' : "Als een ingredient voor ontwerpen",
+    'blueprint' : "Blauwdruk",
+    'bugs' : "Bugs",
+    'crafting' : "Ontwerpen",
+    'damage and function times' : "Schade en functie tijden",
+    'external links' : "Externe links",
+    'gallery' : "Galerij",
+    'item set' : "Voorwerp set",
+    'notes' : "nota's",
+    'painted variants' : "Geverfde varianten",
+    'references' : "Verwijzingen",
+    'related achievements' : "Gerelateerde prestaties",
+    'see also' : "Zie ook",
+    'strange variant' : "vVeemde variant",
+    'styles' : "Stijlen",
+    'trivia' : "Trivia",
+    'unused content' : "Ongebruikte inhoud",
+    'update history' : "Update verleden",
+}
+
+#ITEM LEVELS
+DICTIONARY_LEVEL_C = {
+    'Apparel' : "Kledij",
+    'Armband' : "Armband",
+    'Aura of Incorruptibility' : "Aura van Oncorruptheid",
+    'Backpack' : "Rugzak",
+    'Badge' : "Badge",
+    'Balloon' : "Ballon",
+    'Bandages' : "Verband",
+    'Bandana' : "Bandana",
+    'Bandolier' : "Patronengordel",
+    'Barbeque' : "Barbeque",
+    'Beach Towel' : "Strand Handdoek",
+    'Bells' : "Belletjes",
+    'Bird Head' : "Vogel Hoofd",
+    'Blueprints' : "Blauwdrukken",
+    'Bones' : "Beenderen",
+    'Bongos' : "Bongo's",
+    'Boots' : "Botten",
+    'Botkiller' : "Botkiller",
+    'Cape' : "Cape",
+    'Championship Belt' : "Kampioenschaps Gordel",
+    'Cigar' : "Sigaar",
+    'Coat' : "Jas",
+    'Coffin' : "Doodskist",
+    'Community Medal' : "Community Medaille",
+    'Conscience' : "Bewustzijn",
+    'Cooler' : "Koeler",
+    'Cosmetic Armor' : "Cosmetisch Pantser",
+    'Cosmetic Augmentation' : "Cosmetische augmentatie",
+    'Cosmetic Axe' : "Cosmetische Bijl",
+    'Cosmetic Knife' : "Cosmetisch Mes",
+    'Costume Piece' : "Kostuum Stuk",
+    'Decorative Bombs' : "Decoratieve Bommen",
+    'Duck' : "Eend",
+    'Electronic Device' : "Elektronisch Apparaat",
+    'Eye Stalks' : "Oog Stengels",
+    'Facial Hair' : "Gezichtsbeharing",
+    'Flair!' : "Flair!",
+    'Flip-Flops' : "Flip-Flops",
+    'Fuel Tank' : "Brandstoftank",
+    'Func_Medal' : "Functie medaille",
+    'Futuristic Sound Device' : "Futuristisch Geluidsapparaat",
+    'Ghost' : "Geest",
+    'Glasses' : "Bril",
+    'Glove' : "Handschoen",
+    'Gloves' : "Handschoenen",
+    'Golf Clubs' : "Golf Clubs",
+    'Hair' : "Haar",
+    'Hat' : "Hoed",
+    'Headgear' : "Hoofddeksel",
+    'Headset' : "Koptelefoon",
+    'Helmet' : "Helm",
+    'Holiday Hat' : "Feestdag Hoed",
+    'Hooves' : "Hoeven",
+    'Kilt' : "Kilt",
+    'Lantern' : "Lantaarn",
+    'Lunchbox' : "Lunchtrommel",
+    'Mascot' : "Mascotte",
+    'Mask' : "Masker",
+    'Medal' : "Medaille",
+    'Medallion' : "Medaillon",
+    'Mystical Lamp' : "Mystieke Lamp",
+    'Necklace' : "Halsketting",
+    'Photograph' : "Foto",
+    'Pin' : "Pin",
+    'Pipe' : "Pijp",
+    'Pocket Buddy' : "Zak Vriendje",
+    'Pocket Square' : "Zak Vierkant",
+    'Poncho' : "Poncho",
+    'Puffy Shirt' : "Opgezwollen Shirt",
+    'Pyrovision Goggles' : "Pyrovisie bril",
+    'Refreshment' : "Verfrissing",
+    'Ring' : "Ring",
+    'Robot' : "Robot",
+    'Safety Apparatus' : "Veiligheidsmiddel",
+    'Satchel' : "Zakje",
+    'Scarf' : "Sjaal",
+    'Science Project' : "Wetenschappelijk Project",
+    'Shield' : "Schild",
+    'Shirt' : "Shirt",
+    'Shoes' : "Schoenen",
+    'Skateboard' : "Skateboard",
+    'Sled' : "Slee",
+    'Snow Globe' : "Sneeuwbal",
+    'Spikes' : "Spikes",
+    'Spirit Animal' : "",
+    'Spooky Companion' : "Spookig gezelschap",
+    'Spurs' : "Sporen",
+    'Squirrel' : "Eekhoorn",
+    'Stethoscope' : "Stethoscoop",
+    'Stocking' : "Kous",
+    'Supplies' : "Voorraden",
+    'Tattoos' : "Tattoos",
+    'Tentacles' : "Tentakels",
+    'Tournament Medal' : "Toernooi Medaille",
+    'Towel' : "Handdoek",
+    'Treasure' : "Schat",
+    'Tuxedo' : "Tuxedo",
+    'Undead Pet' : "Ondood Huisdier",
+    'Uniform' : "Uniform",
+    "Veteran's Beret" : "Veteranen Barret",
+    'Wings' : "Vleugels",
+}

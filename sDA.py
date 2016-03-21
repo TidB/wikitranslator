61d1417d964f0c6f293260a4c5b25ce08a75d409
Til Eichholz <tidb@gmx.net>
2016-03-21 23:16:07 +0100

    Added Danish

---
+++ sDA.py
@@ -0,0 +1,199 @@
+#NAME
+LANGUAGE_NAME = "Danish"
+
+#STANDARD
+ADDEDTOGAME = "* Denne genstand blev tilføjet til spillet."
+ALLCLASSESBOX = "[[All classes/da|Alle klasser]]"
+DISPLAYTITLE = "{{{{DISPLAYTITLE: {{{{item name|{0}}}}}}}}}"
+ITEMLOOK = " Den fremstår som"
+LEVEL = "Niveau {0} {1}"
+NOUNMARKER_INDEFINITE_COSMETIC = "(Navneordsmarkering)"
+NOUNMARKER_INDEFINITE_SET = "(Navneordsmarkering)"
+NOUNMARKER_INDEFINITE_WEAPON = "(Navneordsmarkering)"
+
+SENTENCE_1_ALL = "(NOUN MARKER) '''{{{{item name|{0}}}}}''' er {1}{2}{3} {4} til {5}."
+SENTENCE_1_COMMUNITY_COSMETIC = "[[Steam Workshop/da|Fællesskabs-lavet]]"
+SENTENCE_1_COMMUNITY_WEAPON = "[[Steam Workshop/da|Fællesskabs-lavet]]"
+SENTENCE_1_PROMO_COSMETIC = "[[Promotional items/da|promoverings]]"
+SENTENCE_1_PROMO_WEAPON = "[[Promotional items/da|promoverings]]"
+SENTENCE_1_COSMETIC = "[[Cosmetic item/da|kosmetisk genstand]]"
+SENTENCE_1_SET = "[[Item set/da|genstandssæt]]"
+SENTENCE_1_CLASSES_ALL = "alle [[Classes/da|klasser]]"
+SENTENCE_1_CLASSES_ONE = "[[{0}]]en"
+SENTENCE_1_CLASSES_MORE = "og [[{0}]]en."
+SENTENCE_1_CLASSES_AND = " og "
+SENTENCE_1_CLASSES_COMMA = ", "
+
+SENTENCE_COMMUNITY = "Denne genstand blev{1} til [[Steam Workshop/da|Steam-værkstedet]]{2}."
+SENTENCE_COMMUNITY_LINK = " [{0} bidraget]"
+SENTENCE_COMMUNITY_NAME = " under navnet \"{0}\""
+
+SENTENCE_PROMOTIONAL = "Denne genstand er tildelt i [[Genuine/da|Ægte]] kvalitet til spillere, som købte ''[[{1}/da|{1}]]''{2}{3}."
+SENTENCE_PROMOTIONAL_STEAM = " på [[Steam/da|Steam]]"
+SENTENCE_PROMOTIONAL_DATE = " før {0}"
+
+SENTENCE_SET = " Den blev tilføjet til spillet i {{{{Update link|{0}}}}}en."
+SENTENCE_SET_INCLUDES = "Sættet inkluderer følgende genstande:"
+
+SENTENCE_THUMBNAIL = "|Steam-værkstedets illustration af denne genstand."
+
+SENTENCE_1_SUB_PRIMARY = "[[Weapon/da#{0}primary|primære våben]]"
+SENTENCE_1_SUB_SECONDARY = "[[Weapon/da#{0}secondary|sekundære våben]]"
+SENTENCE_1_SUB_MELEE = "[[Weapon/da#{0}melee|nærkampsvåben]]"
+
+#FLAGS
+DICTIONARY_FLAGS = {
+    "not usable in crafting" : "kan ikke bruges i smedearbejde",
+    "not tradable" : "Kan ikke byttes",
+    "not tradable or usable in crafting" : "Kan ikke byttes eller bruges I smedearbejde",
+    }
+
+#ATTRIBUTES
+DICTIONARY_ATTS = {
+    "achievement item: not tradable" : "Præstationsgenstand: Kan ikke byttes",
+    "holiday restriction: tf birthday" : "Højtidsbegrænsning: TF-fødselsdag",
+    "holiday restriction: winter" : "Højtidsbegræsning: Vinter",
+    "holiday restriction: halloween" : "Højtidsbegræsning: Halloween",
+    "holiday restriction: halloween / full moon" : "Højtidsbegræsning: Halloween/Fuldmåne",
+    "holiday restriction: halloween / full moon / valentine's day" : "Højtidsbegræsning: Halloween/Fuldmåne/Valentinsdag",
+    }
+
+#CLASSES
+DICTIONARY_CLASSES = {
+
+}
+
+#HEADLINES
+DICTIONARY_HEADLINES = {
+    'as a crafting ingredient' : "Som en ingrediens til smedearbejde",
+    'blueprint' : "Arbejdstegning",
+    'bugs' : "Fejl",
+    'crafting' : "Smedearbejde",
+    'damage and function times' : "Skade og funktion-tider",
+    'external links' : "Eksterne links",
+    'gallery' : "Galleri",
+    'item set' : "Genstandssæt",
+    'notes' : "Noter",
+    'painted variants' : "Malede varianter",
+    'references' : "",
+    'related achievements' : "Relateret præstationer",
+    'see also' : "Se også",
+    'strange variant' : "Sær variant",
+    'styles' : "Stile",
+    'trivia' : "Paratviden",
+    'unused content' : "Ikke-brugt indhold",
+    'update history' : "Opdateringshistorik",
+    }
+
+#ITEM LEVELS
+DICTIONARY_LEVEL_C = {
+    'Apparel' : "Beklædning",
+    'Apron' : "Forklæde",
+    'Armband' : "Armbånd",
+    'Aura of Incorruptibility' : "Aura af Ubestikkelighed",
+    'Backpack' : "Rygsæk",
+    'Badge' : "Emblem",
+    'Balloon' : "Ballon",
+    'Bandages' : "Bandager",
+    'Bandana' : "Bandana",
+    'Bandolier' : "",
+    'Barbeque' : "Barbeque",
+    'Beach Towel' : "Strandhåndklæde",
+    'Bells' : "Klokker",
+    'Bird Head' : "Fuglehoved",
+    'Blueprints' : "Planer",
+    'Bones' : "Knogler",
+    'Bongos' : "Bongoer",
+    'Boots' : "Støvler",
+    'Botkiller' : "Botkiller",
+    'Bottles' : "Flasker",
+    'Cape' : "Kappe",
+    'Championship Belt' : "Mesterskabsbælte",
+    'Cigar' : "Cigar",
+    'Coat' : "Frakke",
+    'Coffin' : "Kiste",
+    'Community Medal' : "Fællesskabsmedalje",
+    'Conscience' : "Samvittighed",
+    'Cooler' : "Køler",
+    'Cosmetic Armor' : "Kosmetisk rustning",
+    'Cosmetic Augmentation' : "Kosmetisk augmentering",
+    'Cosmetic Axe' : "Kosmetisk økse",
+    'Cosmetic Knife' : "kosmetiskm kniv",
+    'Costume Piece' : "Kostumestykke",
+    'cursed soul' : "Forbandet sjæl",
+    'Decorative Bombs' : "Dekorative bomber",
+    'Duck' : "And",
+    'Electronic Device' : "Elektronisk enhed",
+    'Eye Patch' : "Øjeklap",
+    'Eye Stalks' : "",
+    'Facial Hair' : "Ansigtsbehåring",
+    'Flair!' : "Stil",
+    'Flip-Flops' : "Klip-klapper",
+    'Fuel Tank' : "Brændstofstank",
+    'Func_Medal' : "",
+    'Futuristic Sound Device' : "Futuristisk lydenhed",
+    'Ghost' : "Spøgelse",
+    'Glasses' : "Briller",
+    'Glove' : "Handske",
+    'Gloves' : "Handsker",
+    'Golf Clubs' : "Golfkøller",
+    'Hair' : "Hår",
+    'Hat' : "Hat",
+    'Headgear' : "Hovedudstyr",
+    'Headset' : "Headset",
+    'Helmet' : "Hjelm",
+    'Holiday Hat' : "Helligdagshat",
+    'Holster': "Hylster",
+    'Hooves' : "Hove",
+    'Kilt' : "Kilt",
+    'Lantern' : "Lanterne",
+    'Lunchbox' : "Madkasse",
+    'Mascot' : "Maskot",
+    'Mask' : "Maske",
+    'Medal' : "Medalje",
+    'Medallion' : "Medaljon",
+    'Mystical Lamp' : "Mystisk lampe",
+    'Necklace' : "Halskæde",
+    'Party Favor' : "Festtjeneste",
+    'Photograph' : "Fotografi",
+    'Pin' : "Knap",
+    'Pipe' : "Pibe",
+    'Pocket Buddy' : "Lommeven",
+    'Pocket Square' : "Lommefirkant",
+    'Poncho' : "Poncho",
+    'Puffy Shirt' : "Oppustet skjorte",
+    'Pyrovision Goggles' : "Pyrosynsbriller",
+    'quiver': "Pilekogger",
+    'Refreshment' : "Forfriskning",
+    'Ring' : "Ring",
+    'Robot' : "Robot",
+    'Safety Apparatus' : "Sikkerhedsapparet",
+    'Satchel' : "Taske",
+    'Scarf' : "Halstørklæde",
+    'Science Project' : "",
+    'Shield' : "Skjold",
+    'Shirt' : "Trøje",
+    'Shoes' : "Sko",
+    'Skateboard' : "Skateboard",
+    'Sled' : "Slæde",
+    'Snow Globe' : "Snekugle",
+    'Spikes' : "Pigge",
+    'Spirit Animal' : "Åndedyr",
+    'Spooky Companion' : "Uhyggelig ledsager",
+    'Spurs' : "Sporer",
+    'Squirrel' : "Egern",
+    'Stethoscope' : "Stetoskop",
+    'Stocking' : "Strømper",
+    'Supplies' : "Forsyninger",
+    'Tattoos' : "Tatoveringer",
+    'Tentacles' : "Tentakler",
+    'Tool Belt' : "Værktøjsbælte",
+    'Tournament Medal' : "Turneringsmedalje",
+    'Towel' : "Håndtørklæde",
+    'Treasure' : "Skat",
+    'Tuxedo' : "Tuxedo",
+    'Undead Pet' : "Udødt kæledyr",
+    'Uniform' : "Uniform",
+    "Veteran's Beret" : "Veteranens Beret ",
+    'Wings' : "Vinger",
+    }
